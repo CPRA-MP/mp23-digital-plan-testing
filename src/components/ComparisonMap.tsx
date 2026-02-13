3a0ba6315da68b1f05ebe808e322e6ff63842fea
Matt Yoder <mjyoder@psc.edu>
2026-02-13 09:32:30 -0500

    fix(map): only render map in the browser

--- src/components/ComparisonMap.tsx
+++ src/components/ComparisonMap.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
-import { Map as GLMap } from "maplibre-gl";
-import GLCompare from "@maplibre/maplibre-gl-compare";
+
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 import "maplibre-gl/dist/maplibre-gl.css";
 import "@maplibre/maplibre-gl-compare/dist/maplibre-gl-compare.css";
@@ -11,41 +11,49 @@ export default function ComparisonMap({
   center = [-91.5, 29.75],
   zoom = 7,
 }) {
-  const container = useRef(null);
-  useEffect(() => {
-    if (!container.current) return;
-
-    const beforeMap = new GLMap({
-      container: "before",
-      style: beforeUrl,
-      center,
-      zoom,
-    });
-
-    const afterMap = new GLMap({
-      container: "after",
-      style: afterUrl,
-      center,
-      zoom,
-    });
-
-    const map = new GLCompare(beforeMap, afterMap, container.current, {});
-  }, [container.current]);
-
-  const mapStyle = {
-    bottom: 0,
-    position: "absolute",
-    top: 0,
-    width: "100%",
-  };
-
   return (
-    <div
-      ref={container}
-      style={{ height: "500px", position: "relative", width: "100%" }}
-    >
-      <div id="before" className="map" style={mapStyle}></div>
-      <div id="after" className="map" style={mapStyle}></div>
-    </div>
+    <BrowserOnly>
+      {() => {
+        const GLMap = require("maplibre-gl").Map;
+        const GLCompare = require("@maplibre/maplibre-gl-compare");
+        const container = useRef(null);
+        useEffect(() => {
+          if (!container.current) return;
+
+          const beforeMap = new GLMap({
+            container: "before",
+            style: beforeUrl,
+            center,
+            zoom,
+          });
+
+          const afterMap = new GLMap({
+            container: "after",
+            style: afterUrl,
+            center,
+            zoom,
+          });
+
+          const map = new GLCompare(beforeMap, afterMap, container.current, {});
+        }, [container.current]);
+
+        const mapStyle = {
+          bottom: 0,
+          position: "absolute",
+          top: 0,
+          width: "100%",
+        };
+
+        return (
+          <div
+            ref={container}
+            style={{ height: "500px", position: "relative", width: "100%" }}
+          >
+            <div id="before" className="map" style={mapStyle}></div>
+            <div id="after" className="map" style={mapStyle}></div>
+          </div>
+        );
+      }}
+    </BrowserOnly>
   );
 }
